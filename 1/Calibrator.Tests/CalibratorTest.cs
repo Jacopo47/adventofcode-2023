de4bd007e69ea156f2e9eeb9254616f86d979419
Jacopo <jacopo.riciputi@gmail.com>
2023-12-02 14:53:14 +0100

    Day 1: second exercise

--- 1/Calibrator.Tests/CalibratorTest.cs
+++ 1/Calibrator.Tests/CalibratorTest.cs
@@ -3,12 +3,12 @@ namespace Calibrator.Test;
 public class CalibratorTest
 {
     [Fact]
-    public void WorksFromStaticInputList()
+    public void PartOne_ResolvesExample()
     {
         var input = new List<string>() { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet" };
 
 
-        Assert.Equal(142, new Calibrator().sumOfCalibrations(input));
+        Assert.Equal(142, Calibrator.SumOfCalibrations(input));
     }
 
     [Fact]
@@ -17,15 +17,38 @@ public class CalibratorTest
         var input = new List<string>() { "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet", null };
 
 
-        Assert.Equal(0, new Calibrator().sumOfCalibrations(null));
-        Assert.Equal(142, new Calibrator().sumOfCalibrations(input));
+        Assert.Equal(0, Calibrator.SumOfCalibrations(null));
+        Assert.Equal(142, Calibrator.SumOfCalibrations(input));
     }
 
     [Fact]
-    public void WorksFromStaticInputList_3()
+    public void PartOne_ResolvesExercise()
     {
         var calibrator = new Calibrator();
 
-        Assert.Equal(55607, calibrator.sumOfCalibrations(calibrator.loadCalibrationsFromFile("res/input.txt")));
+        Assert.Equal(55607, Calibrator.SumOfCalibrations(calibrator.loadCalibrationsFromFile("res/input.txt")));
+    }
+
+    [Fact]
+    public void PartTwo_ResolvesExample()
+    {
+        var input = new List<string>() { "two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen" };
+
+        Assert.Equal(281, Calibrator.SumOfCalibrations(input, Calibrator.ReplaceSpelledDigits));
+
+
+        Assert.Equal(12, Calibrator.SumOfCalibrations(new List<string>() {"oneeighttwo"}, Calibrator.ReplaceSpelledDigits));
+        Assert.Equal(12, Calibrator.SumOfCalibrations(new List<string>() {"oneightwo"}, Calibrator.ReplaceSpelledDigits));
+
+        Assert.Equal(18, Calibrator.SumOfCalibrations(new List<string>() {"oneight"}, Calibrator.ReplaceSpelledDigits));
+
+  }
+
+    [Fact]
+    public void PartTwo_ResolvesExercise()
+    {
+        var calibrator = new Calibrator();
+
+        Assert.Equal(55291, Calibrator.SumOfCalibrations(calibrator.loadCalibrationsFromFile("res/input.txt"), Calibrator.ReplaceSpelledDigits));
     }
 }
--- 1/Calibrator/Calibrator.cs
+++ 1/Calibrator/Calibrator.cs
@@ -2,6 +2,22 @@
 
 public class Calibrator
 {
+
+  static readonly IEnumerable<KeyValuePair<string, string>> SPELLED_DIGITS = new Dictionary<string, string>() {
+    { "one" , "1" },
+    { "two" , "2" },
+    { "three" , "3" },
+    { "four" , "4" },
+    { "five" , "5" },
+    { "six" , "6" },
+    { "seven" , "7" },
+    { "eight" , "8" },
+    { "nine" , "9" }
+  }.ToList();
+  static readonly Func<string, string> DO_NOT_REPLACE_SPELLED_DIGITS = _ => _;
+
+
+
   readonly Func<string, IEnumerable<string>> readFile = File.ReadLines;
 
 
@@ -10,15 +26,42 @@ public class Calibrator
     return readFile.Invoke(path);
   }
 
-  public int sumOfCalibrations(IEnumerable<string> input)
+  public static int SumOfCalibrations(IEnumerable<string> input, Func<string, string>? unsafeReplaceSpelledDigits = null)
   {
+    var replaceSpelledDigits = unsafeReplaceSpelledDigits ?? DO_NOT_REPLACE_SPELLED_DIGITS;
+
     return (input ?? Enumerable.Empty<string>())
       .Where(_ => _ != null)
+      .Select(replaceSpelledDigits)
       .Select(chars => chars.Where(Char.IsDigit))
       .Select(_ => _.ToList())
       .Where(_ => _.Count > 0)
       .Select(_ => $"{_.First()}{_.Last()}")
       .Select(int.Parse)
       .Sum();
+  }
+
+  public static string ReplaceSpelledDigits(string input)
+  {
+    return ReplaceSpelledDigits(input, SPELLED_DIGITS);;
+  }
+
+  public static string ReplaceSpelledDigits(string input, IEnumerable<KeyValuePair<string, string>>? toReplace = null)
+  {
+    var toReplaceSafe = toReplace ?? SPELLED_DIGITS;
+
+    if (!toReplaceSafe.Any()) return input;
+
+    var element = toReplaceSafe.First();
+    /* While replacing I'm keeping the "key" value so the spelled value in order to not lose information.
+      In case the last letter of a number is the first of the next one, like:
+       - eightwothree
+       - xtwone3four
+    */
+    var inputWithAppliedReplace = input.Replace(element.Key, $"{element.Key}{element.Value}{element.Key}");
+
+    if (toReplaceSafe.Count() == 1) return inputWithAppliedReplace;
+
+    return ReplaceSpelledDigits(inputWithAppliedReplace, toReplaceSafe.Skip(1));
   }
 }
